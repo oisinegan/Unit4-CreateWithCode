0da42720f1f7bb2c0addf016f109df0ac24928d9
oisinegan <oisinegan00@gmail.com>
2021-10-28 15:32:06 +0100

    Add Code Repository button
    Button to bring player to github repository of game

---
+++ Prototype4/Assets/Scripts/AddCodeLink.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCodeLink : MonoBehaviour
+{
+  public void AddCodeRepo()
+    {
+        Application.OpenURL("https://github.com/oisinegan/Unit4-CreateWithCode");
+    }
+
+}
